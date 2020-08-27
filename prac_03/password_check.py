9d011fa5f7b5af3fb51ee98871af9c42fb1eef50
samanthamartin4700 <samantha.martin@my.jcu.edu>
2020-08-27 19:58:27 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,5 +1,18 @@
-password = input("Enter password (minimum 8 characters): ")
-while len(password) < 8:
-    print("Incorrect password. Your password must be more than 8 characters long!")
+def main():
+    password = get_password()
+    show_asterix(password)
+
+
+def show_asterix(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter password (minimum 8 characters): ")
-print("*" * len(password))
+    while len(password) < 8:
+        print("Incorrect password. Your password must be more than 8 characters long!")
+        password = input("Enter password (minimum 8 characters): ")
+    return password
+
+
+main()
