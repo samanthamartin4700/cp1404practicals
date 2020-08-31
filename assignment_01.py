f89f3d034acb53f34d7237941f457254167f963c
samanthamartin4700 <samantha.martin@my.jcu.edu>
2020-08-31 20:00:23 +1000

    Initial commit

---
+++ assignment_01.py
@@ -0,0 +1,12 @@
+"""
+Replace the contents of this module docstring with your own details
+Name:
+Date started:
+GitHub URL:
+"""
+
+
+def main():
+    print("Travel Tracker 1.0 - by Samantha Martin")
+
+main()
