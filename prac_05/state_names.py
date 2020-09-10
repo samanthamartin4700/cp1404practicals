7f68cb188d4f56bd0dbd5f87c8f26d3c98d40a3a
samanthamartin4700 <samantha.martin@my.jcu.edu>
2020-09-10 12:03:53 +1000

    Add loop to print state names neatly.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,12 +7,15 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+# print(CODE_TO_NAME)
+#
+# state_code = input("Enter short state: ").upper()
+# while state_code != "":
+#     if state_code in CODE_TO_NAME:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+for abbreviations in CODE_TO_NAME:
+    print("{:<3} is {}".format(abbreviations, CODE_TO_NAME[abbreviations]))
