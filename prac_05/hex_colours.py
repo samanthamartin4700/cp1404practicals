aef937b4b0a60ac1dbaa7816d4c42d866c76b0fe
samanthamartin4700 <samantha.martin@my.jcu.edu>
2020-09-10 16:11:17 +1000

    Stop program from running when the user enters blank input.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,8 @@
+"""
+CP1404 HEX COLOURS
+By Samantha Martin
+"""
+
 COLOURS = {"coral": "#ff7f50", "DarkSalmon": "#e9967a", "DarkSeaGreen2": "#b433b4", "OliveDrab2": "#b3ee3a",
            "pale": "#db7093", "PaleGreen3": "#7ccd7c", "PaleTurquoise3": "#96cdcd", "PaleVioletRed": "#db7093",
            "pink": "#ffc0cb", "plum": "#dda0dd"}
@@ -5,7 +10,11 @@ COLOURS = {"coral": "#ff7f50", "DarkSalmon": "#e9967a", "DarkSeaGreen2": "#b433b
 colour_name = input("Enter colour name: ")
 for name in COLOURS:
     if colour_name in COLOURS:
-        print(COLOURS[name])
-    else:
+        print(COLOURS[colour_name])
+        colour_name = input("Enter colour name: ")
+    elif colour_name == "":
+        print("Goodbye :)")
+        break
+    elif colour_name not in COLOURS:
         print("Invalid name. Please try again :)")
-    colour_name = input("Enter colour name: ")
+        colour_name = input("Enter colour name: ")
